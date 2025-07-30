c1a29b5df29ccda00d6c60af9882a6fd39b68283
Michael Frank <msfrank@syntaxjockey.com>
2025-07-30 15:11:12 -0700

    initial commit
    * initialize uv project
    * add pelican dependency
    * use 'creative' bootstrap theme as the base

---
+++ site/pelicanconf.py
@@ -0,0 +1,30 @@
+AUTHOR = 'Michael Frank'
+SITENAME = 'Gracchus Consulting'
+SITEURL = 'gracchusconsulting.com'
+
+PATH = 'content'
+STATIC_PATHS = ['images']
+
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = 'en'
+
+THEME = 'gracchus'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = ()
+
+# Social widget
+SOCIAL = ()
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+RELATIVE_URLS = True
